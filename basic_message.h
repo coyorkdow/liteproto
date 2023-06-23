ff1665bd25c46a32ec8b2a6aa86969c7142ef2d3
youtao.guo <coyorkdow@gmail.com>
2023-06-23 23:51:41 +0800

    scratch

--- basic_message.h
+++ basic_message.h
@@ -4,10 +4,46 @@
 
 #pragma once
 
+#include <array>
 #include <iostream>
 #include <type_traits>
 
-template <class Msg, int32_t end>
-constexpr auto GetAllFields(int32_t c) {
+template <class Tp, size_t... S1, size_t... S2>
+constexpr auto ConcatSTDArray(std::index_sequence<S1...>, std::index_sequence<S2...>,
+                              const std::array<Tp, sizeof...(S1)>& a1, const std::array<Tp, sizeof...(S2)>& a2) {
+  using arr = std::array<Tp, sizeof...(S1) + sizeof...(S2)>;
+  return arr{a1[S1]..., a2[S2]...};
+}
 
+template <class Tp, int32_t Now, int32_t End>
+constexpr decltype(auto) GetAllFieldsImpl() {
+  if constexpr (Now == End) {
+    return std::array<int32_t, 0>{};
+  } else if constexpr (Tp::template FIELD_seq<Now, void> != -1) {
+    constexpr auto remains = GetAllFieldsImpl<Tp, Now + 1, End>();
+    return ConcatSTDArray(std::make_index_sequence<1>{}, std::make_index_sequence<remains.size()>{},
+                          std::array<int32_t, 1>{Now}, remains);
+  } else {
+    return GetAllFieldsImpl<Tp, Now + 1, End>();
+  }
+}
+
+template <class Tp>
+constexpr decltype(auto) GetAllFields() {
+  constexpr auto res1 = GetAllFieldsImpl<Tp, Tp::FIELDS_start, Tp::FIELDS_start + 128>();
+  constexpr auto res2 = GetAllFieldsImpl<Tp, Tp::FIELDS_start + 128, Tp::FIELDS_start + 256>();
+  constexpr auto res3 = GetAllFieldsImpl<Tp, Tp::FIELDS_start + 256, Tp::FIELDS_start + 384>();
+  constexpr auto res4 = GetAllFieldsImpl<Tp, Tp::FIELDS_start + 384, Tp::FIELDS_start + 512>();
+  constexpr auto concat1 = ConcatSTDArray(std::make_index_sequence<res1.size()>{},
+                                          std::make_index_sequence<res2.size()>{},
+                                          res1,
+                                          res2);
+  constexpr auto concat2 = ConcatSTDArray(std::make_index_sequence<res3.size()>{},
+                                          std::make_index_sequence<res4.size()>{},
+                                          res3,
+                                          res4);
+  return ConcatSTDArray(std::make_index_sequence<concat1.size()>{},
+                        std::make_index_sequence<concat2.size()>{},
+                        concat1,
+                        concat2);
 }
--- main.cpp
+++ main.cpp
@@ -1,77 +1,57 @@
 #include <iostream>
 #include <type_traits>
 
+#include "basic_message.h"
+
 template <int32_t N>
 using int32_constant = std::integral_constant<int32_t, N>;
 
-#define FIELD(name)                                                            \
-  name##_;                                                                     \
-                                                                               \
- public:                                                                       \
-  const decltype(name##_)& name() const { return name##_; }                    \
-  decltype(name##_)& mutable_##name() { return name##_; }                      \
-  void set_##name(decltype(name##_) v) { name##_ = std::move(v); }             \
-  static constexpr auto FIELD_name(int32_constant<__LINE__>) { return #name; } \
-  auto FIELD_type(int32_constant<__LINE__>)->decltype(test_field_);            \
-  decltype(name##_)& FIELD_value(int32_constant<__LINE__>) { return name##_; } \
-  template <>                                                                  \
-  static constexpr int32_t FIELD_seq<__LINE__>
+#define FIELD(name)                                                                      \
+  name##_;                                                                               \
+                                                                                         \
+ public:                                                                                 \
+  const decltype(name##_)& name() const { return name##_; }                              \
+  decltype(name##_)& mutable_##name() { return name##_; }                                \
+  void set_##name(decltype(name##_) v) { name##_ = std::move(v); }                       \
+  static constexpr decltype(auto) FIELD_name(int32_constant<__LINE__>) { return #name; } \
+  auto FIELD_type(int32_constant<__LINE__>)->decltype(test_field_);                      \
+  decltype(name##_)& FIELD_value(int32_constant<__LINE__>) { return name##_; }           \
+  template <class Tp>                                                                    \
+  static constexpr int32_t FIELD_seq<__LINE__, Tp>
 
-#define DECLARE_FIELDS                     \
- public:                                   \
-  template <int32_t N>                     \
-  static constexpr int32_t FIELD_seq = -1; \
-                                           \
+#define DECLARE_FIELDS                              \
+ public:                                            \
+  template <int32_t N, class>                       \
+  static constexpr int32_t FIELD_seq = -1;          \
+  static constexpr int32_t FIELDS_start = __LINE__; \
+                                                    \
  private:
 
-class TestMessage {
+#define MESSAGE(name) class name
+
+MESSAGE(TestMessage) {
   DECLARE_FIELDS
 
   int FIELD(test_field) = 0;
   double FIELD(test_field2) = 1;
-
-  //  int test_field_;
-  //  const decltype(test_field_)& test_field() const { return test_field_; }
-  //  void set_test_field(decltype(test_field_) v) { test_field_ = std::move(v); }
-  //  static constexpr decltype(auto) FIELD_name(int32_constant<11>) { return "test_field"; }
-  //  auto FIELD_type(int32_constant<11>) -> decltype(test_field_);
-  //  template <>
-  //  static constexpr auto FIELD_seq<11> = 0;
 };
 
-template <class Tp, size_t... S1, size_t... S2>
-constexpr auto ConcatSTDArray(std::index_sequence<S1...>, std::index_sequence<S2...>,
-                              const std::array<Tp, sizeof...(S1)>& a1, const std::array<Tp, sizeof...(S2)>& a2) {
-  using arr = std::array<Tp, sizeof...(S1) + sizeof...(S2)>;
-  return arr{a1[S1]..., a2[S2]...};
-}
-
-template <int32_t Now, int32_t End>
-constexpr decltype(auto) GetAllFields() {
-  if constexpr (Now == End) {
-    return std::array<int32_t, 0>{};
-  } else if constexpr (TestMessage::FIELD_seq<Now> != -1) {
-    constexpr auto remains = GetAllFields<Now + 1, End>();
-    return ConcatSTDArray(std::make_index_sequence<1>{}, std::make_index_sequence<remains.size()>{},
-                          std::array<int32_t, 1>{Now}, remains);
-  } else {
-    return GetAllFields<Now + 1, End>();
+template <size_t N>
+constexpr bool str_literal_eq(const char (&s1)[N], const char (&s2)[N]) {
+  for (size_t i = 0; i < N; i++) {
+    if (s1[i] != s2[i]) return false;
   }
-}
-
-template <class Tp, size_t N>
-size_t Size(Tp (&arr)[N]) {
-  return N;
+  return true;
 }
 
 int main() {
-  constexpr const auto arr = GetAllFields<0, 100>();
+  constexpr const auto arr = GetAllFields<TestMessage>();
   static_assert(arr.size() == 2);
-  //  static_assert(arr[0] == 35);
-  //  static_assert(arr[1] == 36);
-  //  using arr = int32_t[];
-  //  arr t {1, 2, 3};
-  //  std::cout << Size(t);
+  constexpr char test_field_name[] = "test_field";
+  constexpr char test_field2_name[] = "test_field2";
+
+  static_assert(str_literal_eq(TestMessage::FIELD_name(int32_constant<arr[0]>{}), test_field_name));
+  static_assert(str_literal_eq(TestMessage::FIELD_name(int32_constant<arr[1]>{}), test_field2_name));
 
   return 0;
 }
